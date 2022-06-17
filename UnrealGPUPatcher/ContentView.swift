9066d3f849c2178b450bdc989c1da2b21be90da8
Jacob Greenfield <jacob@jacobgreenfield.me>
2022-06-17 15:28:59 -0400

    Initial Commit

---
+++ UnrealGPUPatcher/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnrealGPUPatcher
+//
+//  Created by Jacob Greenfield on 6/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UnrealGPUPatcher/UnrealGPUPatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnrealGPUPatcherApp.swift
+//  UnrealGPUPatcher
+//
+//  Created by Jacob Greenfield on 6/17/22.
+//
+
+import SwiftUI
+
+@main
+struct UnrealGPUPatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
